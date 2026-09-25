058b1926755a8237da9407642b4638ecc830297c
JB957 <joeybird8@outlook.com>
2026-09-24 21:24:37 -0400

    Gitcommits

--- src/app/main.c
+++ src/app/main.c
@@ -9,6 +9,7 @@
 #include "adc.h"
 #include "error_handler.h"
 #include "core_config.h"
+#include "spi.h"
 
 #include "FreeRTOS.h"
 #include "queue.h"
@@ -17,18 +18,21 @@
 
 #include <stm32g4xx_hal.h>
 
-
-void heartbeat_task(void *pvParameters) {
-    (void) pvParameters;
-    while(true) {
+void heartbeat_task(void *pvParameters)
+{
+    (void)pvParameters;
+    while (true)
+    {
         core_GPIO_toggle_heartbeat();
         vTaskDelay(200 * portTICK_PERIOD_MS);
     }
 }
 
-void port14_task(void *pvParameters) {
-    (void) pvParameters;
-    while(true) {
+void port14_task(void *pvParameters)
+{
+    (void)pvParameters;
+    while (true)
+    {
         core_GPIO_digital_write(GPIOB, GPIO_PIN_14, true);
         vTaskDelay(300 * portTICK_PERIOD_MS);
         core_GPIO_digital_write(GPIOB, GPIO_PIN_14, false);
@@ -36,9 +40,11 @@ void port14_task(void *pvParameters) {
     }
 }
 
-void port15_task(void *pvParameters) {
-    (void) pvParameters;
-    while(true) {
+void port15_task(void *pvParameters)
+{
+    (void)pvParameters;
+    while (true)
+    {
         core_GPIO_digital_write(GPIOB, GPIO_PIN_15, true);
         vTaskDelay(1000 * portTICK_PERIOD_MS);
         core_GPIO_digital_write(GPIOB, GPIO_PIN_15, false);
@@ -46,63 +52,135 @@ void port15_task(void *pvParameters) {
     }
 }
 
-void Print_Port12(void *pvParameters) {
-    (void) pvParameters;
+void Print_Port12(void *pvParameters)
+{
+    (void)pvParameters;
     uint16_t adc_value;
-    while(true) {
-        if (core_ADC_read_channel(GPIOB, GPIO_PIN_12, &adc_value)) {
+    while (true)
+    {
+        if (core_ADC_read_channel(GPIOB, GPIO_PIN_12, &adc_value))
+        {
             rprintf("PB12 ADC: %u\n", adc_value);
         }
         vTaskDelay(100 * portTICK_PERIOD_MS);
     }
 }
 
-int main(void) {
+void lightToFreq(void *pvParameters)
+{
+    (void)pvParameters;
+    uint16_t adc_value;
+    while (true)
+    {
+        core_ADC_read_channel(GPIOB, GPIO_PIN_12, &adc_value);
+        if (adc_value <= 2000 && adc_value >= 0)
+        {
+            core_GPIO_digital_write(GPIOB, GPIO_PIN_15, true);
+            core_GPIO_digital_write(GPIOB, GPIO_PIN_14, true);
+            core_GPIO_digital_write(GPIOB, GPIO_PIN_13, true);
+            vTaskDelay((100 * 10) * portTICK_PERIOD_MS);
+            core_GPIO_digital_write(GPIOB, GPIO_PIN_14, false);
+            core_GPIO_digital_write(GPIOB, GPIO_PIN_13, false);
+            core_GPIO_digital_write(GPIOB, GPIO_PIN_15, false);
+            vTaskDelay((100 * 10) * portTICK_PERIOD_MS);
+        }
+    }
+}
+
+// void SendCanMessage(void *pvParameters)
+// {
+//     (void)pvParameters;
+//     while (true)
+//     {
+//         uint64_t data = 250;
+
+//         core_CAN_send_message(FDCAN1, 10, 8, data);
+//     }
+// }
+
+void SendSPI(void *pvParameters)
+{
+    (void)pvParameters;
+    while (true)
+    {
+        core_SPI_start(SPI1);
+        core_SPI_read_write(SPI1, )
+    }
+}
+
+int main(void)
+{
     HAL_Init();
 
     // Drivers
-
     core_heartbeat_init(GPIOB, GPIO_PIN_13);
     core_GPIO_init(GPIOB, GPIO_PIN_14, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL);
     core_GPIO_init(GPIOB, GPIO_PIN_15, GPIO_MODE_OUTPUT_PP, GPIO_NOPULL);
     core_GPIO_init(GPIOB, GPIO_PIN_11, GPIO_MODE_INPUT, GPIO_NOPULL);
-    if (!core_ADC_init(ADC1)) error_handler();
-    if (!core_ADC_setup_pin(GPIOB, GPIO_PIN_12, 0)) error_handler();
+    if (!core_CAN_init(FDCAN1, 1000000))
+    {
+        error_handler();
+    }
+
+    if (!core_SPI_init(SPI1, GPIOA, GPIO_PIN_4))
+        error_handler();
+    if (!core_ADC_init(ADC1))
+        error_handler();
+    if (!core_ADC_setup_pin(GPIOB, GPIO_PIN_12, 0))
+        error_handler();
 
     core_GPIO_digital_write(GPIOB, GPIO_PIN_13, false);
     core_GPIO_digital_write(GPIOB, GPIO_PIN_14, false);
     core_GPIO_digital_write(GPIOB, GPIO_PIN_15, false);
-            
-    
-    if (!core_clock_init()) error_handler();
-    if (!core_CAN_init(FDCAN1, 1000000)) error_handler();
 
-    while (1) {
-    if (core_GPIO_digital_read(GPIOB, GPIO_PIN_11)) {
-        break;   // start action here
-        }
-    }
-
-    int err = xTaskCreate(heartbeat_task, "heartbeat", 1000, NULL, 4, NULL);
-    if (err != pdPASS) {
+    if (!core_clock_init())
         error_handler();
-    }
-
-    int err2 = xTaskCreate(port14_task, "port14", 1000, NULL, 4, NULL);
-    if (err2 != pdPASS) {
+    if (!core_CAN_init(FDCAN1, 1000000))
         error_handler();
-    }
 
-    int err3 = xTaskCreate(port15_task, "port15", 1000, NULL, 4, NULL);
-    if (err3 != pdPASS) {
-        error_handler();
-    }
+    // while (1)
+    // {
+    //     if (core_GPIO_digital_read(GPIOB, GPIO_PIN_11))
+    //     {
+    //         break; // start action here
+    //     }
+    // }
+
+    // int err = xTaskCreate(heartbeat_task, "heartbeat", 1000, NULL, 4, NULL);
+    // if (err != pdPASS)
+    // {
+    //     error_handler();
+    // }
+
+    // int err2 = xTaskCreate(port14_task, "port14", 1000, NULL, 4, NULL);
+    // if (err2 != pdPASS)
+    // {
+    //     error_handler();
+    // }
+
+    // int err3 = xTaskCreate(port15_task, "port15", 1000, NULL, 4, NULL);
+    // if (err3 != pdPASS)
+    // {
+    //     error_handler();
+    // }
 
     int err4 = xTaskCreate(Print_Port12, "adc_pb12", 1000, NULL, 4, NULL);
-    if (err4 != pdPASS) {
+    if (err4 != pdPASS)
+    {
         error_handler();
     }
 
+    int err5 = xTaskCreate(lightToFreq, "light_to_freq", 1000, NULL, 4, NULL);
+    if (err5 != pdPASS)
+    {
+        error_handler();
+    }
+
+    // int err6 = xTaskCreate(SendCanMessage, "can", 1000, NULL, 4, NULL);
+    // if (err6 != pdPASS)
+    // {
+    //     error_handler();
+    // }
 
     NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
 
@@ -116,9 +194,10 @@ int main(void) {
 
 // Called when stack overflows from rtos
 // Not needed in header, since included in FreeRTOS-Kernel/include/task.h
-void vApplicationStackOverflowHook( TaskHandle_t xTask, char *pcTaskName) {
-    (void) xTask;
-    (void) pcTaskName;
+void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName)
+{
+    (void)xTask;
+    (void)pcTaskName;
 
     error_handler();
 }
